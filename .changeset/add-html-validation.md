247e449830df43945a8005abcba1014613de1986
alexgagnon <>
2025-07-17 06:46:30 -0400

    tweaks

---
+++ .changeset/add-html-validation.md
@@ -0,0 +1,5 @@
+---
+"@ttl-helpers/lib": minor
+---
+
+Added options for validating CSS, HTML, and SVG, either consumer defined in the `ttl-helpers/validate/*v.js` exports, or defaults using lightningcss, html-validate, and fast-xml-parser libraries in the `ttl-helpers/validate/*.js` exports
--- packages/extension/test/index.js
+++ packages/extension/test/index.js
@@ -10,6 +10,14 @@ console.log(css`.hello {
   }`}
 }`);
 
+css`@media screen (max-width: 600px) {
+  .hello {
+    color: red;
+    line-height: 1.5;
+    
+  }
+}`
+
 console.log(html`<!doctype html><head></head><body><h1 style="padding: ${padding}">Hello!</h1></body></html>`);
 
 console.log(svg`<svg width="100" height="100"><circle cx="50" cy="50" r="40" fill="green"/></svg>`);
