108788805be458cdba302eaa73d3cb73f515ce44
alexgagnon <>
2025-07-26 16:55:48 -0400

    fixes

--- packages/vscode-extension/test/index.js
+++ packages/vscode-extension/test/index.js
@@ -1,8 +1,25 @@
 import { css, html, xml } from 'ttls';
 
+console.log(`${something}.html`, 'blah');
+console.log(`${something}.css`, 'blah');
+console.log(`${something}.xml`, 'blah');
+
 html`<div class="something" ${spread({ class: 'container' })}>
 <style>
   ${css`svg { fill: blue; }`}
 </style>
 ${xml`<svg width="100" height="100"><circle cx="50" cy="50" r="40" /></svg>`}
 </div>`;
+
+css`
+.containers { 
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+  ${true ? css`
+    background-color: lightblue;
+  ` : css`
+    background-color: lightcoral;
+  `}
+}`;
